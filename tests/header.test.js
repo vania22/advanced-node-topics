9d2ebf1c99dcd37b9da3cd35ae938f0732152302
Ivan Krupskyi <krupskiy111@gmail.com>
2021-03-13 02:05:21 +0000

    Added custom page class

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,19 +1,16 @@
-const puppeteer = require('puppeteer');
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
+const Page = require('./helpers/page');
 
-let browser, page;
+let page;
 
 beforeEach(async () => {
-    browser = await puppeteer.launch({ headless: false });
-    page = await browser.newPage();
+    page = await Page.build();
     await page.setViewport({ width: 1080, height: 1024, deviceScaleFactor: 1 });
 
     await page.goto('http://localhost:3000');
 });
 
 afterEach(async () => {
-    await browser.close();
+    await page.close();
 });
 
 test('Verify logo text', async () => {
@@ -30,15 +27,8 @@ test('Check that user taken to google when clicks login button', async () => {
 });
 
 test.only('Logout button present if user logged in', async () => {
-    // const userId = '60478183feb5da3128e26438';
-    const user = await userFactory();
-    console.log(user);
-    const { session, sig } = sessionFactory(user);
+    await page.login();
 
-    await page.setCookie({ name: 'session', value: session });
-    await page.setCookie({ name: 'session.sig', value: sig });
-    await page.goto('http://localhost:3000');
-    await page.waitForSelector('a[href="/auth/logout"]');
     const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerText);
 
     expect(text).toEqual('Logout');
